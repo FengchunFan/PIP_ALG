1fff56be3f39a7f01dae389f64302c1ead015eac
FengchunFan <ffan005@ucr.edu>
2023-12-10 15:10:06 -0800

    added non inplace scan

--- scan/scan.cpp
+++ scan/scan.cpp
@@ -19,24 +19,42 @@ int main(int argc, char *argv[]) {
   }
   Type *seq_scan_A = (Type *)malloc(n * sizeof(Type));
   Type *pal_scan_A = (Type *)malloc(n * sizeof(Type));
+  Type *pal_scan_B = (Type *)malloc(n * sizeof(Type));
   parallel_for(0, n, [&](size_t i) {
     seq_scan_A[i] = i;
     pal_scan_A[i] = i;
+    pal_scan_B[i] = i;
   });
 
   double total_time = 0;
-  std::cout << "******* Parallel Results: ******* " << std::endl;
+  std::cout << "******* Parallel In Place Results: ******* " << std::endl;
   for (int i = 0; i <= num_rounds; i++) {
     parlay::timer t;
-    long long exclusive_sum = scan(pal_scan_A, n);
+    long long exclusive_sum = scan_in_place(pal_scan_A, n);
     t.stop();
     if (i == 0) {
-      std::cout << "Exclusive sum (Parallel): " << exclusive_sum << std::endl;
+      std::cout << "Exclusive sum (Parallel In Place): " << exclusive_sum << std::endl;
       std::cout << "Warmup round running time: " << t.total_time() << std::endl;
     } else {
       std::cout << "Round " << i << " running time: " << t.total_time()
                 << std::endl;
       total_time += t.total_time();
+    }
+  }
+
+  double total_time_2 = 0;
+  std::cout << "******* Parallel Non In Place Results: ******* " << std::endl;
+  for (int i = 0; i <= num_rounds; i++) {
+    parlay::timer t;
+    long long exclusive_sum = scan(pal_scan_B, n);
+    t.stop();
+    if (i == 0) {
+      std::cout << "Exclusive sum (Parallel non in place): " << exclusive_sum << std::endl;
+      std::cout << "Warmup round running time: " << t.total_time() << std::endl;
+    } else {
+      std::cout << "Round " << i << " running time: " << t.total_time()
+                << std::endl;
+      total_time_2 += t.total_time();
     }
   }
 
@@ -61,7 +79,9 @@ int main(int argc, char *argv[]) {
   std::cout << "\n******* Summary: ******* " << std::endl;
   std::cout << "Average sequential running time: "
             << seq_total_time / num_rounds << std::endl;
-  std::cout << "Average parallel running time: " << total_time / num_rounds
+  std::cout << "Average parallel in place running time: " << total_time / num_rounds
+            << std::endl;
+  std::cout << "Average parallel non in place running time: " << total_time_2 / num_rounds
             << std::endl;
   
   /*std::cout << "seq results: ";
@@ -75,7 +95,7 @@ int main(int argc, char *argv[]) {
   }
   std::cout << std::endl;*/
   for (size_t i = 0; i < n; i++) {
-    if (seq_scan_A[i] != pal_scan_A[i]) {
+    if (seq_scan_A[i] != pal_scan_A[i] || pal_scan_A[i] != pal_scan_B[i]) {
       std::cout << "***********************" << std::endl;
       std::cout << "**** Wrong answer! ****" << std::endl;
       std::cout << "***** index: "<< i << " ****" << std::endl;
@@ -86,5 +106,6 @@ int main(int argc, char *argv[]) {
 
   free(seq_scan_A);
   free(pal_scan_A);
+  free(pal_scan_B);
   return 0;
 }
--- scan/scan.h
+++ scan/scan.h
@@ -7,23 +7,28 @@ using namespace parlay;
 using Type = long long;
 
 template <typename T>
-T scan_up(T *A, size_t s, size_t t);
+T scan_up_in_place(T *A, size_t s, size_t t);
 template <typename T>
-void scan_down(T *A, size_t s, size_t t, T offset);
+void scan_down_in_place(T *A, size_t s, size_t t, T offset);
+
+template <typename T>
+T scan_up(T *A, T *LS, size_t n);
+template <typename T>
+void scan_down(T *A, T *LS, size_t n, T offset);
 
 /**
  * Inplace block scan DIRECTLY without filter
  */
 template <typename T>
-T scan(T *A, size_t n) {
+T scan_in_place(T *A, size_t n) {
   T total = 0;
-  total = scan_up(A, 0, n-1);
-  scan_down(A, 0, n-1, (T)0);
+  total = scan_up_in_place(A, 0, n-1);
+  scan_down_in_place(A, 0, n-1, (T)0);
   return total;
 }
 
 template <typename T>
-T scan_up(T *A, size_t s, size_t t) {
+T scan_up_in_place(T *A, size_t s, size_t t) {
   T temp = A[t];
   if(t - s + 1 < 1e5){
     for(size_t i = s; i < t; ++i){
@@ -34,15 +39,15 @@ T scan_up(T *A, size_t s, size_t t) {
   }
   //if(s==t)return A[s];
   T v1, v2;
-  auto f1 = [&]() { v1 = scan_up(A, s, (s+t)/2); };
-  auto f2 = [&]() { v2 = scan_up(A, (s+t)/2 + 1, t); };
+  auto f1 = [&]() { v1 = scan_up_in_place(A, s, (s+t)/2); };
+  auto f2 = [&]() { v2 = scan_up_in_place(A, (s+t)/2 + 1, t); };
   par_do(f1, f2);
   A[t] = v1 + v2;
   return v1 + v2;
 }
 
 template <typename T>
-void scan_down(T *A, size_t s, size_t t, T offset) {
+void scan_down_in_place(T *A, size_t s, size_t t, T offset) {
   if(t - s + 1 < 1e5){
     T temp = 0;
     T p = offset;
@@ -58,7 +63,59 @@ void scan_down(T *A, size_t s, size_t t, T offset) {
     return;
   }*/
   T leftsum = A[(s+t)/2];
-  auto f1 = [&]() {scan_down(A, s, (s+t)/2, offset);};
-  auto f2 = [&]() {scan_down(A, (s+t)/2 + 1, t, offset + leftsum);};
+  auto f1 = [&]() {scan_down_in_place(A, s, (s+t)/2, offset);};
+  auto f2 = [&]() {scan_down_in_place(A, (s+t)/2 + 1, t, offset + leftsum);};
   par_do(f1, f2);
+}
+
+template <typename T>
+T scan(T *A, size_t n) {
+  T total = 0;
+  Type *LS = (Type *)malloc((n-1) * sizeof(Type));
+
+  total = scan_up(A, LS, n);
+
+  scan_down(A, LS, n, (T)0);
+
+  free(LS);
+  return total;
+}
+
+//builds	a	tree	of	sums, only left branch involved
+//similar to a reduce function but store information
+template <typename T>
+T scan_up(T *A, T *LS, size_t n) {
+  if (n < 1e5){
+    T total = 0;
+    for (size_t i = 0; i < n; i++) {
+      total += A[i];
+    }
+    return total;
+  }else{
+    size_t m = n/2;
+    T v1, v2;
+    auto f1 = [&]() { v1 = scan_up(A, LS, m); };
+    auto f2 = [&]() { v2 = scan_up(A + m, LS + m, n - m); };
+    par_do(f1, f2);
+    LS[m-1] = v1;
+    return v1 + v2;
+  }
+}
+
+//traverses	the	tree to	compute	prefixes
+template <typename T>
+void scan_down(T *A, T *LS, size_t n, T offset) {
+  if(n < 1e5){
+    T total = 0;
+    for (size_t i = 0; i < n; i++) {
+      T tmp = A[i];
+      A[i] = total + offset;
+      total += tmp;
+    }
+  }else{
+    size_t m = n/2;
+    auto f1 = [&]() {scan_down(A, LS, m, offset);};
+    auto f2 = [&]() {scan_down(A + m, LS + m, n - m, offset + LS[m-1]);};
+    par_do(f1, f2);
+  }
 }

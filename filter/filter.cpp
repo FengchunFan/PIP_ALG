b67c8c99cd32bafcc7406cd5249c921f29ad22b4
FengchunFan <ffan005@ucr.edu>
2023-12-10 20:07:33 -0800

    finished non-in-place filter

--- filter/filter.cpp
+++ filter/filter.cpp
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
   //size_t n = 1e4;
   int num_rounds = 0;
   long long pal_ans = 0;
+  long long pal2_ans = 0;
   long long seq_ans = 0;
   if (argc >= 2) {
     n = atoll(argv[1]);
@@ -21,24 +22,43 @@ int main(int argc, char *argv[]) {
   }
   Type *seq_filter_A = (Type *)malloc(n * sizeof(Type));
   Type *pal_filter_A = (Type *)malloc(n * sizeof(Type));
+  Type *pal_filter_B = (Type *)malloc(n * sizeof(Type));
+
   parallel_for(0, n, [&](size_t i) {
     seq_filter_A[i] = i;
     pal_filter_A[i] = i;
+    pal_filter_B[i] = i;
   });
 
   double total_time = 0;
-  std::cout << "******* Parallel Results: ******* " << std::endl;
+  std::cout << "******* Parallel In Place Results: ******* " << std::endl;
   for (int i = 0; i <= num_rounds; i++) {
     parlay::timer t;
-    pal_ans = pal_filter(pal_filter_A, n);
+    pal_ans = pal_filter_in_place(pal_filter_A, n);
     t.stop();
     if (i == 0) {
-      std::cout << "Number of selected elements (Parallel): " << pal_ans << std::endl;
+      std::cout << "Number of selected elements (in place Parallel): " << pal_ans << std::endl;
       std::cout << "Warmup round running time: " << t.total_time() << std::endl;
     } else {
       std::cout << "Round " << i << " running time: " << t.total_time()
                 << std::endl;
       total_time += t.total_time();
+    }
+  }
+
+  double total_time_2 = 0;
+  std::cout << "\n******* Parallel Non In Place Results: ******* " << std::endl;
+  for (int i = 0; i <= num_rounds; i++) {
+    parlay::timer t;
+    pal2_ans = pal_filter_non_in_place(pal_filter_B, n);
+    t.stop();
+    if (i == 0) {
+      std::cout << "Number of selected elements (non in place Parallel): " << pal2_ans << std::endl;
+      std::cout << "Warmup round running time: " << t.total_time() << std::endl;
+    } else {
+      std::cout << "Round " << i << " running time: " << t.total_time()
+                << std::endl;
+      total_time_2 += t.total_time();
     }
   }
 
@@ -60,10 +80,13 @@ int main(int argc, char *argv[]) {
       seq_total_time += seq_t.total_time();
     }
   }
+
   std::cout << "\n******* Summary: ******* " << std::endl;
   std::cout << "Average sequential running time: "
             << seq_total_time / 1 << std::endl;
-  std::cout << "Average parallel running time: " << total_time / 1
+  std::cout << "Average parallel in place running time: " << total_time / 1
+            << std::endl;
+  std::cout << "Average parallel non in place running time: " << total_time_2 / 1
             << std::endl;
   
   /*std::cout << "seq results: ";
@@ -79,9 +102,9 @@ int main(int argc, char *argv[]) {
   std::cout << "seq ans: " << seq_ans << ". pal ans: " << pal_ans;
   std::cout << std::endl;*/
 
-  if(seq_ans == pal_ans){
+  if(seq_ans == pal_ans && pal_ans == pal2_ans){
     for (size_t i = 0; i < (size_t)seq_ans - 1; i++) {
-      if (seq_filter_A[i] != pal_filter_A[i]) {
+      if (seq_filter_A[i] != pal_filter_A[i] || pal_filter_B[i] != pal_filter_A[i]) {
         std::cout << "***********************" << std::endl;
         std::cout << "**** Wrong answer! ****" << std::endl;
         std::cout << "***** index: "<< i << " ****" << std::endl;
@@ -95,5 +118,6 @@ int main(int argc, char *argv[]) {
 
   free(seq_filter_A);
   free(pal_filter_A);
+  free(pal_filter_B);
   return 0;
 }
--- filter/filter.h
+++ filter/filter.h
@@ -12,9 +12,30 @@ template <class T>
 T sequ_filter(T *A, T *output, size_t n);
 template <class T>
 T seq_filter(T *A, size_t n);
+template <typename T>
+T scan(T *A, size_t n, T *LS);
+template <typename T>
+T scan_up(T *A, T *LS, size_t n);
+template <typename T>
+void scan_down(T *A, T *LS, size_t n, T offset);
 
 template <class T>
-T pal_filter(T *A, size_t n) {
+T reduce(T *A, size_t n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return A[0];
+  } else {
+    T v1, v2;
+    auto f1 = [&]() { v1 = reduce(A, n / 2); };
+    auto f2 = [&]() { v2 = reduce(A + n / 2, n - n / 2); };
+    par_do(f1, f2);
+    return v1 + v2;
+  }
+}
+
+template <typename T>
+T pal_filter_in_place(T *A, size_t n) {
   T total = 0;
   if(n < 1e5){
     total = sequ_filter(A, A, n);
@@ -36,10 +57,7 @@ T pal_filter(T *A, size_t n) {
 		Sums[i] = k - s;
   });
 
-  std::atomic<int> counter(0);
-  for(size_t i = 0; i < l; ++i){
-    counter.fetch_add(Sums[i], std::memory_order_relaxed);
-  }
+  total = reduce(Sums,l);
 
   size_t start = 0;
   for(size_t i = 0; i < l; ++i){
@@ -51,7 +69,43 @@ T pal_filter(T *A, size_t n) {
 
   free(Sums);
 
-  return counter.load();;
+  return total;
+}
+
+template <typename T>
+T pal_filter_non_in_place(T *A, size_t n) {
+  T total = 0;
+  if(n < 1e5){
+    total = sequ_filter(A, A, n);
+    return total;
+  }
+  
+  Type *flag = (Type *)malloc(n * sizeof(Type));
+  Type *B = (Type *)malloc(n * sizeof(Type));
+  Type *LS = (Type *)malloc((n) * sizeof(Type));
+
+  parallel_for(0, n, [&](size_t i) {
+    flag[i] = (A[i] % 10 == 0);
+  });
+
+  scan((T*)flag, n, LS);
+
+  parallel_for(0, n, [&](size_t i) {
+    if(A[i] % 10 == 0){
+      B[flag[i]] = A[i];
+    }
+  });
+
+  parallel_for(0, n, [&](size_t i) {
+    A[i] = B[i];
+  });
+
+  total = flag[n-1];
+
+  free(flag);
+  free(B);
+  free(LS);
+  return total;
 }
 
 template <class T>
@@ -72,4 +126,53 @@ T sequ_filter(T *A, T *output, size_t n) {
     A[i] = output[i];
   });
   return k;
+}
+
+template <typename T>
+T scan(T *A, size_t n, T *LS) {
+  T total = 0;
+
+  total = scan_up(A, LS, n);
+  scan_down(A, LS, n, (T)0);
+
+  return total;
+}
+
+//builds	a	tree	of	sums, only left branch involved
+//similar to a reduce function but store information
+template <typename T>
+T scan_up(T *A, T *LS, size_t n) {
+  if (n < 1e5){
+    T total = 0;
+    for (size_t i = 0; i < n; i++) {
+      total += A[i];
+    }
+    return total;
+  }else{
+    size_t m = n/2;
+    T v1, v2;
+    auto f1 = [&]() { v1 = scan_up(A, LS, m); };
+    auto f2 = [&]() { v2 = scan_up(A + m, LS + m, n - m); };
+    par_do(f1, f2);
+    LS[m-1] = v1;
+    return v1 + v2;
+  }
+}
+
+//traverses	the	tree to	compute	prefixes
+template <typename T>
+void scan_down(T *A, T *LS, size_t n, T offset) {
+  if(n < 1e5){
+    T total = 0;
+    for (size_t i = 0; i < n; i++) {
+      T tmp = A[i];
+      A[i] = total + offset;
+      total += tmp;
+    }
+  }else{
+    size_t m = n/2;
+    auto f1 = [&]() {scan_down(A, LS, m, offset);};
+    auto f2 = [&]() {scan_down(A + m, LS + m, n - m, offset + LS[m-1]);};
+    par_do(f1, f2);
+  }
 }
